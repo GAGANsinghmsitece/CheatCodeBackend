bc116fdfc6d27b26dd51207e208845d94b698c13
Gagan Singh <gagan.s@smarter.codes>
2022-07-10 17:42:55 +0530

    improving signup

--- backend/cheatcode/serializers.py
+++ backend/cheatcode/serializers.py
@@ -59,9 +59,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create(
             username=validated_data['username'],
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
+            email=validated_data['email']
         )
 
         
